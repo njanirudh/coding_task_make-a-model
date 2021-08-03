682d5bd418892e2a27606cfa290e0a109bfcc64d
njanirudh <anijaya9@gmail.com>
2021-08-03 17:06:04 +0200

    Not working!

---
+++ src/seg_trainer.py
@@ -0,0 +1,169 @@
+from pprint import pprint
+
+import pytorch_lightning as pl
+import segmentation_models_pytorch as smp
+import torch
+import numpy as np
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+
+logger = TensorBoardLogger("tb_logs", name="my_model")
+
+from utils.checkpoint_utils import PeriodicCheckpoint
+
+from src.pidata.pidata import pi_parser
+from src.utils.custom_config import custom_parser_config
+
+# Setting seed for reproducibility
+seed = 666
+torch.manual_seed(seed)
+
+Tensor = torch.tensor
+Module = torch.nn.Module
+
+
+class SegmentationModule(pl.LightningModule):
+    """
+    Pytorch Lightning module for training the
+    UNet segmentation model.
+    """
+
+    def __init__(self, config_data: dict, train_mode: bool = True,
+                 batch_size: int = 10, gpu: int = 1,
+                 epochs: int = 50, lr: float = 0.003) -> None:
+        """
+        :param config_data: Config for data, utils.
+        :param train_mode: Sets model to training or inference mode.
+        :param batch_size: Batch size during training.
+        :param epochs: Total epochs for training. (default 50)
+        :param lr: Learning rate (default 0.003)
+        :param gpu: Set total gpus to use (default 1)
+        """
+        super(SegmentationModule, self).__init__()
+
+        # Testing custom model
+        # self.model = UNET(3, 3)
+        # self.model.train(train_mode)  # Set training mode = true
+
+        # Model from 'segmentation_models_pytorch' library
+        self.model = smp.Unet(
+            encoder_name="mobilenet_v2",  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights="imagenet",  # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=3,  # model output channels (number of classes in your dataset)
+        )
+        self.model.train(train_mode)
+
+        self.val_loader, self.train_loader = None, None
+        self.num_train_imgs, self.num_val_imgs = None, None
+        self.trainer, self.curr_device = None, None
+
+        # Model checkpoint saving every 500 steps
+        self.periodic_chkp = PeriodicCheckpoint(500)
+
+        self.loss_fn = CrossEntropyLoss()
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.learning_rate = lr
+        self.gpu = gpu
+
+    def forward(self, input: Tensor, **kwargs) -> Tensor:
+        return self.model(input)
+
+    def training_step(self, batch, batch_idx):
+        print("==>",len(batch))
+        inputs, labels = batch
+        # inputs, labels = input_tensor, target_dict['semantics']
+        # self.curr_device = inputs.device
+
+        # print(inputs.shape, labels.shape)
+
+        outputs = self.forward(inputs)
+        train_loss = self.loss_fn(outputs, labels)
+
+        return train_loss
+
+    def validation_step(self, batch, batch_idx):
+        print("==>",len(batch))
+
+        inputs, labels = batch
+        # inputs, labels = input_tensor, target_dict['semantics']
+        # self.curr_device = inputs.device
+
+        # print(inputs.shape, labels.shape)
+
+        outputs = self.forward(inputs)
+        val_loss = self.loss_fn(outputs, labels)
+
+        return val_loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        return optimizer
+
+    def train_dataloader(self):
+        training_data_parser = pi_parser.PiParser(
+            config=custom_parser_config,
+            split_name="train",
+            num_samples=10,  # number of samples to be drawn, set to a multiple of the batch size
+            numpy_to_tensor_func=None,
+            # framework-dependent, e.g. torch.from_numpy (PyTorch), if None, the returned type is numpy.ndarray
+        )
+
+        self.train_loader = DataLoader(training_data_parser,
+                                       batch_size=self.batch_size,
+                                       shuffle=True,
+                                       num_workers=4,
+
+                                       )
+        self.num_train_imgs = len(self.train_loader)
+        return self.train_loader
+
+    def val_dataloader(self):
+        val_data_parser = pi_parser.PiParser(
+            config=custom_parser_config,
+            split_name="train",
+            num_samples=10,  # number of samples to be drawn, set to a multiple of the batch size
+            numpy_to_tensor_func=None,
+            # framework-dependent, e.g. torch.from_numpy (PyTorch), if None, the returned type is numpy.ndarray
+        )
+
+        self.val_loader = DataLoader(val_data_parser,
+                                     batch_size=self.batch_size,
+                                     shuffle=True,
+                                     num_workers=4,
+
+                                     )
+        self.num_val_imgs = len(self.val_loader)
+        return self.val_loader
+
+    def train_model(self):
+        self.trainer = pl.Trainer(gpus=self.gpu, max_epochs=self.epochs,
+                                  callbacks=self.periodic_chkp)
+        self.trainer.fit(self,
+                         self.train_dataloader(),
+                         self.val_dataloader())
+
+    def collate_fn(self, batch):
+        pprint(len(batch))
+
+        input_data_list = []
+        label_list = []
+        for item in batch:
+            input_tensor, target_dict = item
+            # print(type(input_tensor), type(target_dict))
+            # print(len(input_tensor), len(target_dict))
+            inputs, labels = input_tensor, target_dict['semantics']
+            input_data_list.append(inputs)
+            label_list.append(labels)
+        zipped = zip(input_data_list, label_list)
+        print("-->", len(list(zipped)[0]))
+        return torch.from_numpy(np.array(input_data_list)), torch.from_numpy(np.array(label_list))
+
+
+if __name__ == "__main__":
+    model_trainer = SegmentationModule(config_data=custom_parser_config,
+                                       batch_size=1,
+                                       epochs=150)
+    model_trainer.train_model()
--- src/utils/custom_config.py
+++ src/utils/custom_config.py
@@ -1,24 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# This file is covered by the LICENSE file in the root of this project.
-
-from __future__ import annotations
-
-import pathlib
-
-import numpy as np
-import cv2
-
 from src.piutils.piutils import pi_log
 from src.piutils.piutils import pi_io
 from src.piutils.piutils import pi_drawing
 from src.pidata.pidata import pi_parser
 
-logger = pi_log.get_logger(__name__)
-
-if __name__ == "__main__":
-
-    parser_config = {
+custom_parser_config = {
         "datasets": {
             "train": [
                 {
@@ -27,8 +12,10 @@ if __name__ == "__main__":
                     "sampling_weight": 1.0,
                 }
             ],
-            "val": [{"path": "TODO/sugarbeet_weed_dataset", "sampling_weight": 1.0}],
-            "test": [{"path": "TODO/sugarbeet_weed_dataset", "sampling_weight": 1.0}],
+            "val": [{"path": "TODO/sugarbeet_weed_dataset",
+                     "sampling_weight": 1.0}],
+            "test": [{"path": "TODO/sugarbeet_weed_dataset",
+                      "sampling_weight": 1.0}],
         },
         "input_layers": [
             {
@@ -68,7 +55,7 @@ if __name__ == "__main__":
                 },  # samples a patch from anywhere within the image
                 {"instances": {"weight": 0.5}},
                 # samples a patch from image regions with plant instances
-                # the frequence of each class is determined by the 'sampling_weight',
+                # the frequency of each class is determined by the 'sampling_weight',
                 # see semantic labels
             ],
             "val": [
@@ -175,79 +162,3 @@ if __name__ == "__main__":
             "test": {},
         },
     }
-
-    train_data_parser = pi_parser.PiParser(
-        config=parser_config,
-        split_name="train",
-        num_samples=100,  # number of samples to be drawn, set to a multiple of teh batch size
-        numpy_to_tensor_func=None,  # framework-dependent, e.g. torch.from_numpy (PyTorch), if None, the returned type is numpy.ndarray
-    )
-
-    drawing_kwargs = dict(
-        mean=train_data_parser.mean,  # undo input normalization
-        std=train_data_parser.std,
-        semantic_labels=train_data_parser.semantic_labels,
-        output_width=train_data_parser.output_width,  # position of network output wrt to input, see pidata.pi_parser.__init__()
-        output_height=train_data_parser.output_height,
-        output_offset_x=train_data_parser.output_offset_x,
-        output_offset_y=train_data_parser.output_offset_y,
-        output_stride_x=train_data_parser.output_stride_x,
-        output_stride_y=train_data_parser.output_stride_y,
-        scale_factor=1.0,
-    )
-
-    for sample_index, (input_tensor, target_dict) in enumerate(train_data_parser):
-
-        logger.info(f"Sample {sample_index+1}/{len(train_data_parser)}.")
-        logger.info(f"    Input shape: {input_tensor.shape}")
-        logger.info(f"    Sampled image IDs: {target_dict['image_id']}")
-
-        logger.info(f"    Targets:")
-        for target_name, target in target_dict.items():
-            logger.info(f"     * '{target_name}':")
-            logger.info(f"       Shape: {target.shape}")
-
-        drawing_input = pi_drawing.draw_input(
-            input_tensor=input_tensor,
-            **drawing_kwargs,
-        )
-        cv2.imshow(
-            "input",
-            drawing_input[..., ::-1],  # RGB to BGR
-        )
-
-        if "semantics" in target_dict:
-            drawing_semantic_labels = (
-                pi_drawing.color_semantic_labels(  # feel free to use in you own code
-                    semantics_tensor=target_dict["semantics"],
-                    **drawing_kwargs,
-                )
-            )
-            cv2.imshow(
-                "semantic_labels",
-                drawing_semantic_labels[..., ::-1],  # RGB to BGR
-            )
-
-        if (
-            "boxes" in target_dict
-            or "keypoints" in target_dict
-            or "masks" in target_dict
-        ) and "labels" in target_dict:
-            drawing_instances = (
-                pi_drawing.draw_instances(  # feel free to use in your own code
-                    input_tensor=input_tensor,
-                    boxes=target_dict["boxes"] if "boxes" in target_dict else None,
-                    keypoints=(
-                        target_dict["keypoints"] if "keypoints" in target_dict else None
-                    ),
-                    masks=target_dict["masks"] if "masks" in target_dict else None,
-                    labels=target_dict["labels"],
-                    **drawing_kwargs,
-                )
-            )
-            cv2.imshow(
-                "instances",
-                drawing_instances[..., ::-1],  # RGB to BGR
-            )
-
-        cv2.waitKey()
